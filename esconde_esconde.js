3e21d6f9bf7a91076316eea3fc2107289558cf56
estheradry <estheradry@hotmail.com>
2021-08-11 01:54:36 -0400

    questão idosos

---
+++ esconde_esconde.js
@@ -0,0 +1,12 @@
+function solucao(numero) {
+	if (numero <= 20) {
+        console.log ("VOCE CONSEGUE, MANINHO")
+    } else if (numero > 20) {
+        
+    } for (let indice = 21; numero >= indice; indice++) {
+        console.log (indice)
+    }
+     
+      
+    
+}
